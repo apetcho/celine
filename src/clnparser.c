1bf72f4b61d317cea1b30f044d19d52971917696
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-03 07:26:15 +0100

    Implement cln_parse()

---
+++ src/clnparser.c
@@ -0,0 +1,54 @@
+#include<assert.h>
+#include "celine.h"
+
+typedef struct {
+    const char *filename;
+    Lexer *lexer;
+    Token currentTokend;
+    Token nextToken;
+} Parser;
+
+// fail_with_unexpected_token()
+// fail_with_parsing_error()
+// advance()
+// match()
+// 
+
+// -*-
+static Ast* _cln_parse_program(Parser *parser);
+static Ast* _cln_parse_block(Parser *parser);
+static Ast* _cln_parse_condition(Parser *parser);
+static Ast* _cln_parse_logical_expr(Parser *parser);
+static Ast* _cln_parse_oplist(Parser *parser);
+static Ast* _cln_parse_op(Parser *parser);
+static Ast* _cln_parse_assign(Parser *parser);
+static Ast* _cln_parse_while(Parser *parser);
+static Ast* _cln_parse_if(Parser *parser);
+static Ast* _cln_parse_call(Parser *parser);
+static Ast* _cln_parse_arglis(Parser *parser);
+static Ast* _cln_parse_expr(Parser *parser);
+static Ast* _cln_parse_artith_expr(Parser *parser);
+static Ast* _cln_parse_term(Parser *parser);
+static Ast* _cln_parse_value(Parser *parser);
+static Ast* _cln_parse_print(Parser *parser);
+static Ast* _cln_parse_def(Parser *parser);
+static Ast* _cln_parse_return(Parser *parser);
+static Ast* _cln_parse_array(Parser *parser);
+static Ast* _cln_parse_object(Parser *parser);
+static Ast* _cln_parse_import(Parser *parser);
+static Ast* _cln_parse_load(Parser *parser);
+
+// -*-
+Ast* cln_parse(const char* filename, Symtable *symtable){
+    Parser parser;
+    parser.filename = filename;
+    parser.lexer = (Lexer*)cln_alloc(sizeof(Lexer));
+    cln_lexer_init(parser.lexer, filename, symtable);
+    Ast *ast = _cln_parse_program(&parser);
+    printf("Symbol ID table: \n");
+    for(int i=0; i < symtable->len; ++i){
+        printf("%d = %s\n", i, symtable->symbols[i]);
+    }
+    cln_dealloc(parser.lexer);
+    return ast;
+}
